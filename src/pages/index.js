7ae9691e0fae9e8895bd09cc0ffb0beb0cf68b4b
Nghia Pham <phmngocnghia@gmail.com>
2020-03-05 20:33:49 +0700

    add tailwind, linaria

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import { css } from 'linaria';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const style = css`
+  @apply bg-red-800
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1 className={style}>Hi people</h1>
+    <p className="bg-red-800">Welcome to your new Gatsby site.</p>
+    <button>Im a button</button>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
